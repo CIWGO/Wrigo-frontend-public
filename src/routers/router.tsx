54d75c32af53e2dbe924fa6b5c805d4a3a0759e6
HAIXIN ZHANG <haixin.zhang@go1.com>
2023-02-28 12:25:35 +0800

    Initialize Project

---
+++ src/routers/router.tsx
@@ -0,0 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+function Router() {
+	return (
+		<BrowserRouter>
+			<Routes>
+				{/* <Route path="/" element={<NavigationPage />} />
+        <Route path="/evaluate" element={<EssayInputPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/verifyEmail" element={<VerifyEmailPage />} />
+        <Route path="/changePassword" element={<ChangePassword />} />
+        <Route path="/UserProfile" element={<UserProfile />} />
+        <Route path="/UpdateUserProfile" element={<UpdateUserProfile />} /> */}
+			</Routes>
+		</BrowserRouter>
+	);
+}
+  
+export default Router;
