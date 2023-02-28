52d0d7ee9529cee2788c6eec1ca0a71489648817
leo <leoyh.97@gmail.com>
2023-02-28 20:48:47 +0800

    Convert TS project to JS project

--- src/index.jsx
+++ src/index.jsx
@@ -5,7 +5,7 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(
-  document.getElementById("root"));
+	document.getElementById("root"));
 root.render(
 	<React.StrictMode>
 		<App />
