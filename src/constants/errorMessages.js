2e567c49702f4cadd244398407b78a9dbcc6fcb7
Shuchen Wu <68723186+ShuchenWuu@users.noreply.github.com>
2023-03-03 00:12:59 +1030

    sign up page finished (#5)
    Add sign up page

---
+++ src/constants/errorMessages.js
@@ -0,0 +1,10 @@
+const ERROR_MESSAGES = {
+	username: "Please input your username!",
+	email: "Please input your email!",
+	emailFormat: "Please enter a valid email address",
+	password: "Please input your password!",
+	passwordConfirm: "Please confirm your password!",
+	passwordMatch: "These two passwords that you entered do not match!"
+};
+
+export default ERROR_MESSAGES;
