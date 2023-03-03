58f0e62155f524342994aa30d067f0d32d80b82c
Chang Liu <changstudy19@gmail.com>
2023-03-03 22:53:58 +1030

    commit for laptop use

--- src/components/SideBar/index.jsx
+++ src/components/SideBar/index.jsx
@@ -1,16 +1,43 @@
 import styled from "styled-components";
+import "./index.scss";
 
 const StyledSideBar = styled.div`
-    height: 100vh;
-    display: flex;
-    flex-direction: column;
-
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+	position: fixed;
+  top: 0;
+  left: 0;
+  background-color: #f5f5f5;
 `;
 
-const SideBar = (props) => {
-	const { children } = props;
+const SideBar = () => {
+	return (
+		<StyledSideBar>
+			<h1 className="title">WRIGO</h1>
 
-	return <StyledSideBar>{children}</StyledSideBar>;
+			<ul className="main">
+				<li>
+					<a href="">DashBoard</a>
+				</li>
+				<li>
+					<a href="">Writings</a>
+				</li>
+				<li>
+					<a href="">Topics</a>
+				</li>
+			</ul>
+
+			<ul className="personal">
+				<li>
+					<a href="">Profile</a>
+				</li>
+				<li>
+					<a href="">Log out</a>
+				</li>
+			</ul>
+		</StyledSideBar>
+	);
 };
 
 export default SideBar;
