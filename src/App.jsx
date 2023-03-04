0383edd74ed1620a1fad9d4e4fc25c7d9277c06d
Lucas Lin <dl.world@hotmail.com>
2023-03-04 15:56:52 +1100

    Landing Page done

--- src/App.jsx
+++ src/App.jsx
@@ -5,28 +5,26 @@ import Router from "./routers/router";
 import { ThemeProvider } from "styled-components";
 
 const theme = {
-	headingSize: "20px",
-	defaultColor: "#2F71DA",
-	headingFontWeight: "700",
+  headingSize: "20px",
+  defaultColor: "#2F71DA",
+  headingFontWeight: "700",
 
-	fontFamily: "Noto Sans",
-	background: "#F5F5F5"
+  fontFamily: "Noto Sans",
+  background: "#F5F5F5",
 };
 
-function App () {
-	const queryClient = new QueryClient();
+function App() {
+  const queryClient = new QueryClient();
 
-	return (
-		<QueryClientProvider client={queryClient}>
-
-			<div className="App">
-				<ThemeProvider theme={theme}>
-					<Router />
-				</ThemeProvider>
-			</div>
-		</QueryClientProvider>
-
-	);
+  return (
+    <QueryClientProvider client={queryClient}>
+      <div className="App">
+        <ThemeProvider theme={theme}>
+          <Router />
+        </ThemeProvider>
+      </div>
+    </QueryClientProvider>
+  );
 }
 
 export default App;
