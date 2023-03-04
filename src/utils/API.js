71348d86ea8c2db164fc83bdbae2b9d27c540392
HAIXIN ZHANG <haixin.zhang@go1.com>
2023-03-05 00:52:01 +0800

    fix conflict

--- src/utils/API.js
+++ src/utils/API.js
@@ -3,4 +3,4 @@ import axios from "axios";
 export const signupUser = (values) => {
 	return axios.post("http://localhost:3005/users/signup", values);
 };
-// your fetching data API could goes here
+// your fetching data API could go here
