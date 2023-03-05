63796c3a97fc55ef745d49997cafc0391a7f871e
chang019 <changstudy19@gmail.com>
2023-03-05 11:51:40 +1100

    commit to pull

--- src/components/SideBar/index.jsx
+++ src/components/SideBar/index.jsx
@@ -1,42 +1,67 @@
-import styled from "styled-components";
+// import styled from "styled-components";
 import "./index.scss";
+import React, { useState } from "react";
 
-const StyledSideBar = styled.div`
-  height: 100vh;
-  display: flex;
-  flex-direction: column;
-	position: fixed;
-  top: 0;
-  left: 0;
-  background-color: #f5f5f5;
-`;
+// const StyledSideBar = styled.div`
+//   height: 100vh;
+//   display: flex;
+//   flex-direction: column;
+// 	position: fixed;
+//   top: 0;
+//   left: 0;
+//   background-color: #f5f5f5;
+// `;
 
 const SideBar = () => {
+	const [activeMenu, setActiveMenu] = useState(0);
+
+	const handleMenuClick = (index) => {
+		setActiveMenu(index);
+	};
+
 	return (
-		<StyledSideBar>
-			<h1 className="title">WRIGO</h1>
+		<div className="side-bar">
+			<h1 className="title"><a href="">WRIGO</a></h1>
 
 			<ul className="main">
-				<li>
-					<a href="">DashBoard</a>
+				<li >
+					<a className={activeMenu === 0 ? "active" : ""}
+						onClick={() => handleMenuClick(0)}
+					>
+						DashBoard
+					</a>
 				</li>
 				<li>
-					<a href="">Writings</a>
+					<a
+						className={activeMenu === 1 ? "active" : ""}
+						onClick={() => handleMenuClick(1)}
+					>
+						Writings
+					</a>
 				</li>
 				<li>
-					<a href="">Topics</a>
+					<a
+						className={activeMenu === 2 ? "active" : ""}
+						onClick={() => handleMenuClick(2)}
+					>
+						Topics
+					</a>
 				</li>
 			</ul>
 
 			<ul className="personal">
 				<li>
-					<a href="">Profile</a>
+					<a
+						className={`${activeMenu === 3 ? "active" : ""}`}
+						onClick={() => handleMenuClick(3)}
+					>
+						Profile
+					</a>
 				</li>
-				<li>
-					<a href="">Log out</a>
-				</li>
+				<li><a href="/">Log out</a></li>
 			</ul>
-		</StyledSideBar>
+
+		</div>
 	);
 };
 
