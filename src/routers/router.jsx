4149360fb09c964e9499a634ad1b4cf618bd458c
Lucas Lin <dl.world@hotmail.com>
2023-03-07 19:16:40 +1100

    router

--- src/routers/router.jsx
+++ src/routers/router.jsx
@@ -1,7 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import DashBoardPage from "../pages/Dashboard/index";
-import WrithingPage from "../pages/Writing/index-old";
-import WritingFirstPage from "../pages/Writing/index";
 import Writing from "../pages/Writing/index";
 import SignUpPage from "../pages/SignUp/index";
 import SubIntroCard from "../components/subIntroCard";
@@ -12,8 +10,6 @@ function Router () {
 		<BrowserRouter>
 			<Routes>
 				<Route path="/dashboard" element={<DashBoardPage />} />
-				<Route path="/writings/first" element={<WritingFirstPage />} />
-				<Route path="/writings/second" element={<WrithingPage />} />
 				<Route path="/writings/*" element={<Writing />} />
 				<Route path="/signup" element={<SignUpPage />} />
 				<Route path="/subintrocard" element={<SubIntroCard />} />
