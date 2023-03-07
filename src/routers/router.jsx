43e93d870101e50cfba8a9e5cf445fc1a5eb74a3
Lucas Lin <dl.world@hotmail.com>
2023-03-07 19:07:13 +1100

    conflict resolved

--- src/routers/router.jsx
+++ src/routers/router.jsx
@@ -2,8 +2,10 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import DashBoardPage from "../pages/Dashboard/index";
 import WrithingPage from "../pages/Writing/index-old";
 import WritingFirstPage from "../pages/Writing/index";
+import Writing from "../pages/Writing/index";
 import SignUpPage from "../pages/SignUp/index";
 import SubIntroCard from "../components/subIntroCard";
+import UserProfile from "../pages/UserProfile/index";
 
 function Router () {
 	return (
@@ -12,8 +14,10 @@ function Router () {
 				<Route path="/dashboard" element={<DashBoardPage />} />
 				<Route path="/writings/first" element={<WritingFirstPage />} />
 				<Route path="/writings/second" element={<WrithingPage />} />
+				<Route path="/writings/*" element={<Writing />} />
 				<Route path="/signup" element={<SignUpPage />} />
 				<Route path="/subintrocard" element={<SubIntroCard />} />
+				<Route path="/userprofile" element={<UserProfile />} />
 			</Routes>
 		</BrowserRouter>
 	);
