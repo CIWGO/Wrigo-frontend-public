c6065662c0cb3698762ecb33b055462d144ae63f
Zihan <zihanzhou1021@gmail.com>
2023-03-08 22:42:29 +1100

    add redux to emailverification

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import numberReducer from "../features/numberSlice";
-
+import emailVerificationReducer from "../features/emailVerificationSlice";
 export const store = configureStore({
 	reducer: {
-		numberStore: numberReducer
+		numberStore: numberReducer,
+		emailVerificationStore: emailVerificationReducer
 		// your store should start form here
 	}
 });
