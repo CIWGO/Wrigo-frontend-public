80315543e1cdd1f4f66f128f6852021ea8d16705
EricSherlockLi <helloleolee@hotmail.com>
2023-03-09 22:08:51 +0800

    Add page and card style

--- src/components/UtilityCard/index.jsx
+++ src/components/UtilityCard/index.jsx
@@ -1,19 +1,46 @@
+// import styled from "styled-components";
+
+// const StyledUtilityCard = styled.div`
+//   background: #ffffff;
+//   border-radius: 20px;
+//   width: 236px;
+//   height: 236px;
+//   padding: 10px;
+//   display: flex;
+//   justify-content: center;
+//   align-items: center;
+// `;
+
+// const UtilityCard = (props) => {
+// 	const { children } = props;
+
+// 	return <StyledUtilityCard>{children}</StyledUtilityCard>;
+// };
+// export default UtilityCard;
+
+import { Card } from "antd";
 import styled from "styled-components";
 
-const StyledUtilityCard = styled.div`
-  background: #ffffff;
-  border-radius: 20px;
-  width: 236px;
-  height: 236px;
-  padding: 10px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+const StyledCard = styled(Card)`
+  && {
+    border-radius: 20px;
+    width: 200px;
+    height: 200px;
+    margin-right: 16px;
+    margin-bottom: 16px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  &&:hover {
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  }
 `;
 
 const UtilityCard = (props) => {
 	const { children } = props;
 
-	return <StyledUtilityCard>{children}</StyledUtilityCard>;
+	return <StyledCard>{children}</StyledCard>;
 };
+
 export default UtilityCard;
