824da2c247ea31cff6bb9aacace1cb351214fff9
leo <leoyh.97@gmail.com>
2023-03-15 20:26:37 +0800

    Modified topic card component and page for data intake

--- src/components/TopicCard/index.jsx
+++ src/components/TopicCard/index.jsx
@@ -4,24 +4,33 @@ import styled from "styled-components";
 const StyledCard = styled(Card)`
 	&& {
 		border-radius: 20px;
-		width: 300px;
-		height: 400px;
+		width: 250px;
+		height: 350px;
 		margin-right: 16px;
 		margin-bottom: 16px;
 		display: inline-block;
+        justify-content: left;
 	}
 	&&:hover {
 		box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
 	}
 `;
 
+const StyledCardContent = styled.div`
+	p {
+		text-align: left;
+	}
+`;
+
 const TopicCard = (props) => {
 	return (
 		<StyledCard>
-			<p>{props.topic}</p>
-			<p>{props.sampleWriting}</p>
-			<p>Sample writing score</p>
-			<p>{props.score}</p>
+			<StyledCardContent>
+				<p>{props.topic}</p>
+				<p>{props.sampleWriting}</p>
+				<p>Sample writing score</p>
+				<p>{props.score}</p>
+			</StyledCardContent>
 		</StyledCard>
 	);
 };
--- src/pages/Topic/index.jsx
+++ src/pages/Topic/index.jsx
@@ -1,19 +1,41 @@
-import React from "react";
+import { getTopic } from "../../utils/index";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import TopicCard from "../../components/TopicCard/index";
+import styled from "styled-components";
 
-const Topic = () => {
+const StyledCardContainer = styled.div`
+	&& {
+		text-align: left;
+		margin: 40px;
+	}
+`;
+
+const TopicMainPage = () => {
+	const [data, setData] = useState([]);
+	useEffect(() => {
+		getTopic({ type: "allTopics" })
+			.then((response) => {
+				if (response.status === 200) {
+					setData(response.data);
+				} else if (response.status === 500) {
+					alert("Something is wrong network, please retry.");
+				}
+			});
+	}, []);
+
 	return (
-		<div>
-			<TopicCard />
-			<TopicCard />
-			<TopicCard />
-			<TopicCard />
-			<TopicCard />
-			<TopicCard />
-			<TopicCard />
-			<TopicCard />
-		</div>
+		<StyledCardContainer>
+			{data.map((item, index) => (
+				<TopicCard
+					key={index}
+					topic={item.topic_content}
+					sampleWriting={item.sampleWriting_content}
+					score={item.score}
+				/>
+			))}
+		</StyledCardContainer>
 	);
 };
 
-export default Topic;
+export default TopicMainPage;
