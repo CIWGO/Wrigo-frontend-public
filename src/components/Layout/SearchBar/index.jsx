667369160602b2069059090dd9f74dd0c8f218f1
EricSherlockLi <helloleolee@hotmail.com>
2023-03-18 22:49:56 +0800

    Search Result 0.1

--- src/components/Layout/SearchBar/index.jsx
+++ src/components/Layout/SearchBar/index.jsx
@@ -1,8 +1,33 @@
-import { Input } from "antd";
+// import { Input } from "antd";
+// import styled from "styled-components";
+
+// const { Search } = Input;
+// const onSearch = (value) => console.log(value);
+// const StyledSearch = styled(Search)`
+//   width: 400px;
+
+//   @media (max-width: 991.98px) {
+//     width: 200px;
+//   }
+// `;
+
+// const SearchBar = () => (
+// 	<StyledSearch
+// 		placeholder="input search text"
+// 		allowClear
+// 		onSearch={onSearch}
+// 	/>
+// );
+
+// export default SearchBar;
+
+import { Input, Select } from "antd";
 import styled from "styled-components";
+import { useState } from "react";
 
 const { Search } = Input;
-const onSearch = (value) => console.log(value);
+const { Option } = Select;
+
 const StyledSearch = styled(Search)`
   width: 400px;
   
@@ -11,12 +36,31 @@ const StyledSearch = styled(Search)`
   }
 `;
 
-const SearchBar = () => (
-	<StyledSearch
-		placeholder="input search text"
-		allowClear
-		onSearch={onSearch}
-	/>
-);
+const SearchBar = () => {
+	const [searchType, setSearchType] = useState("topics");
+
+	const handleSearch = (value) => {
+		console.log(`Searching for "${value}" in ${searchType}`);
+		// 根据选择的搜索类型，构造相应的搜索查询
+	};
+
+	const handleSelectChange = (value) => {
+		setSearchType(value);
+	};
+
+	return (
+		<div>
+			<StyledSearch
+				placeholder="input search text"
+				allowClear
+				onSearch={handleSearch}
+			/>
+			<Select defaultValue="topics" style={{ width: 120, marginLeft: "1em" }} onChange={handleSelectChange}>
+				<Option value="topics">Search Topics</Option>
+				<Option value="writings">Search Writings</Option>
+			</Select>
+		</div>
+	);
+};
 
 export default SearchBar;
