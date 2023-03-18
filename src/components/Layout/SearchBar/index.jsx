e7c0475efda442c2266c9eb7ca01f43edfac9958
EricSherlockLi <helloleolee@hotmail.com>
2023-03-18 23:24:06 +0800

    search with selection 0.2

--- src/components/Layout/SearchBar/index.jsx
+++ src/components/Layout/SearchBar/index.jsx
@@ -21,16 +21,62 @@
 
 // export default SearchBar;
 
+// 2
+// import { Input, Select } from "antd";
+// import styled from "styled-components";
+// import { useState } from "react";
+
+// const { Option } = Select;
+
+// const StyledSearch = styled(Input.Search)`
+//   width: 400px;
+
+//   @media (max-width: 991.98px) {
+//     width: 200px;
+//   }
+// `;
+
+// const SearchBar = () => {
+// 	const [searchType, setSearchType] = useState("topics");
+
+// 	const handleSearch = (value) => {
+// 		console.log(`Searching for "${value}" in ${searchType}`);
+// 		// 根据选择的搜索类型，构造相应的搜索查询
+// 	};
+
+// 	const handleSelectChange = (value) => {
+// 		setSearchType(value);
+// 	};
+
+// 	return (
+// 		<StyledSearch
+// 			placeholder="input search text"
+// 			allowClear
+// 			enterButton
+// 			onSearch={handleSearch}
+// 			addonAfter={
+// 				<Select defaultValue="topics" style={{ width: 120 }} onChange={handleSelectChange}>
+// 					<Option value="topics">Search Topics</Option>
+// 					<Option value="writings">Search Writings</Option>
+// 				</Select>
+// 			}
+// 		/>
+// 	);
+// };
+
+// export default SearchBar;
+
+// 3
 import { Input, Select } from "antd";
 import styled from "styled-components";
 import { useState } from "react";
+import { SearchOutlined } from "@ant-design/icons";
 
-const { Search } = Input;
 const { Option } = Select;
 
-const StyledSearch = styled(Search)`
+const StyledSearch = styled(Input.Search)`
   width: 400px;
-  
+
   @media (max-width: 991.98px) {
     width: 200px;
   }
@@ -41,7 +87,7 @@ const SearchBar = () => {
 
 	const handleSearch = (value) => {
 		console.log(`Searching for "${value}" in ${searchType}`);
-		// 根据选择的搜索类型，构造相应的搜索查询
+		// pending input based on search type selection
 	};
 
 	const handleSelectChange = (value) => {
@@ -49,17 +95,18 @@ const SearchBar = () => {
 	};
 
 	return (
-		<div>
-			<StyledSearch
-				placeholder="input search text"
-				allowClear
-				onSearch={handleSearch}
-			/>
-			<Select defaultValue="topics" style={{ width: 120, marginLeft: "1em" }} onChange={handleSelectChange}>
-				<Option value="topics">Search Topics</Option>
-				<Option value="writings">Search Writings</Option>
-			</Select>
-		</div>
+		<StyledSearch
+			placeholder="Please search for topics or writings"
+			allowClear
+			enterButton={<SearchOutlined />}
+			onSearch={handleSearch}
+			addonBefore={
+				<Select defaultValue="topics" style={{ width: 120 }} onChange={handleSelectChange}>
+					<Option value="topics">Topic</Option>
+					<Option value="writings">Writing</Option>
+				</Select>
+			}
+		/>
 	);
 };
 
