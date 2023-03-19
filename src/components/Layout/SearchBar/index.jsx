feb84eb0f1a909ae8d1380aae0c12bd39e6ae60b
EricSherlockLi <helloleolee@hotmail.com>
2023-03-19 08:33:43 +0800

    search switch 0.1

--- src/components/Layout/SearchBar/index.jsx
+++ src/components/Layout/SearchBar/index.jsx
@@ -67,11 +67,60 @@
 // export default SearchBar;
 
 // 3
-import { Input, Select } from "antd";
+// import { Input, Select } from "antd";
+// import styled from "styled-components";
+// import { useState } from "react";
+
+// const { Option } = Select;
+
+// const StyledSearch = styled(Input.Search)`
+//   width: 400px;
+
+//   @media (max-width: 991.98px) {
+//     width: 200px;
+//   }
+// `;
+
+// const SearchBar = () => {
+// 	const [searchType, setSearchType] = useState("topics");
+
+// 	const handleSearch = (value) => {
+// 		console.log(`Searching for "${value}" in ${searchType}`);
+// 		// 构造相应的搜索查询
+// 		if (searchType === "topics") {
+// 			// 在 topic 数据库中搜索
+// 			console.log("Searching in topics database");
+// 		} else if (searchType === "writings") {
+// 			// 在 writing 内容中搜索
+// 			console.log("Searching in writings content");
+// 		}
+// 	};
+
+// 	const handleSelectChange = (value) => {
+// 		setSearchType(value);
+// 	};
+
+// 	return (
+// 		<StyledSearch
+// 			placeholder="Please search for topics or writings"
+// 			allowClear
+// 			onSearch={handleSearch}
+// 			addonBefore={
+// 				<Select defaultValue="topics" style={{ width: 120 }} onChange={handleSelectChange}>
+// 					<Option value="topics">Topic</Option>
+// 					<Option value="writings">Writing</Option>
+// 				</Select>
+// 			}
+// 		/>
+// 	);
+// };
+
+// export default SearchBar;
+
+// 4
+import { Input, Switch } from "antd";
 import styled from "styled-components";
 import { useState } from "react";
-
-const { Option } = Select;
 
 const StyledSearch = styled(Input.Search)`
   width: 400px;
@@ -84,32 +133,36 @@ const StyledSearch = styled(Input.Search)`
 const SearchBar = () => {
 	const [searchType, setSearchType] = useState("topics");
 
+	const handleSwitchChange = (checked) => {
+		setSearchType(checked ? "writings" : "topics");
+	};
+
 	const handleSearch = (value) => {
 		console.log(`Searching for "${value}" in ${searchType}`);
 		// 构造相应的搜索查询
 		if (searchType === "topics") {
 			// 在 topic 数据库中搜索
-			console.log("Searching in topics database");
+			console.log("Searching in Topics");
 		} else if (searchType === "writings") {
 			// 在 writing 内容中搜索
-			console.log("Searching in writings content");
+			console.log("Searching in Writings");
 		}
-	};
-
-	const handleSelectChange = (value) => {
-		setSearchType(value);
 	};
 
 	return (
 		<StyledSearch
-			placeholder="Please search for topics or writings"
+			placeholder={`Please search for ${searchType === "topics" ? "Topics" : "Writings"
+			}`}
 			allowClear
+			// enterButton
 			onSearch={handleSearch}
 			addonBefore={
-				<Select defaultValue="topics" style={{ width: 120 }} onChange={handleSelectChange}>
-					<Option value="topics">Topic</Option>
-					<Option value="writings">Writing</Option>
-				</Select>
+				<Switch
+					checkedChildren="Writing"
+					unCheckedChildren="Topic"
+					defaultChecked={searchType === "writings"}
+					onChange={handleSwitchChange}
+				/>
 			}
 		/>
 	);
