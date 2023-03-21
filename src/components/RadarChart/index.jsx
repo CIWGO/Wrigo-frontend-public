33ba7d22d723152944afafadf0e061d4aa46aef8
Henry <zihanzhou1021@gmail.com>
2023-03-21 13:47:23 +1100

    add radar chart

---
+++ src/components/RadarChart/index.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+// import { Radar } from "react-chartjs-2";
+
+// const data = {
+// 	labels: ["January", "February", "March", "April", "May", "June", "July"],
+// 	datasets: [
+// 		{
+// 			label: "Sales",
+// 			backgroundColor: "rgba(179,181,198,0.2)",
+// 			borderColor: "rgba(179,181,198,1)",
+// 			pointBackgroundColor: "rgba(179,181,198,1)",
+// 			pointBorderColor: "#fff",
+// 			pointHoverBackgroundColor: "#fff",
+// 			pointHoverBorderColor: "rgba(179,181,198,1)",
+// 			data: [65, 59, 90, 81, 56, 55, 40]
+// 		}
+// 	]
+// };
+
+// const options = {
+// 	scale: {
+// 		ticks: { beginAtZero: true }
+// 	}
+// };
+const RadarChart = () => (
+	<div>
+		<h2>Radar Chart Example</h2>
+		{/* <Radar data={data} options={options} /> */}
+	</div>
+);
+
+export default RadarChart;
