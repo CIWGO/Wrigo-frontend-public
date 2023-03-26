08c711ddd2605bfe15d9524f1cbbe3606b6ada3e
EricSherlockLi <helloleolee@hotmail.com>
2023-03-26 20:14:40 +0800

    fix writing 1.0

--- src/components/WritingCard/index.jsx
+++ src/components/WritingCard/index.jsx
@@ -1,27 +1,60 @@
+/* eslint-disable no-undef */
 import React from "react";
 import { Card } from "antd";
+import styled from "styled-components";
+// import WritingScore from "./WritingScore";
 
 const { Meta } = Card;
 
+const StyledCard = styled(Card)`
+  && {
+    border-radius: 20px;
+    width: 250px;
+    height: 350px;
+    margin-right: 16px;
+    margin-bottom: 16px;
+    display: inline-block;
+    justify-content: left;
+  }
+  &&:hover {
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    cursor: pointer;
+  }
+`;
+
+const StyledCardContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  height: 100%;
+  .score {
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+  }
+  p {
+    text-align: left;
+	word-wrap: break-word;
+	white-space: normal;
+  }
+`;
+
 const WritingCard = ({ taskTopic, writingContent, onClick, selected }) => {
+	const maxLength = 100;
+	const truncatedContent =
+		writingContent.length > maxLength
+			? writingContent.slice(0, maxLength) + "..."
+			: writingContent;
+
 	return (
-		<Card
-			hoverable
-			onClick={onClick}
-			className={selected ? "selected" : ""}
-			style={{ width: "100%" }}
-			cover={
-				<div
-					style={{
-						height: "150px",
-						backgroundColor: "#f5f5f5",
-						borderRadius: "8px 8px 0 0"
-					}}
-				/>
-			}
-		>
-			<Meta title={taskTopic} description={writingContent} />
-		</Card>
+		<StyledCard onClick={onClick} className={selected ? "selected" : ""}>
+			<StyledCardContent>
+				<Meta title={taskTopic} description={truncatedContent} />
+				{/* <div className="score">
+					<WritingScore score={score} />
+				</div> */}
+			</StyledCardContent>
+		</StyledCard>
 	);
 };
 
--- src/pages/WritingMain/myWritingMain.jsx
+++ src/pages/WritingMain/myWritingMain.jsx
@@ -1,17 +1,18 @@
-import React, { useState, useEffect } from "react";
-import styled, { withTheme } from "styled-components";
-import { PlusOutlined } from "@ant-design/icons";
-import { Link } from "react-router-dom";
-import HeadingComponent from "../../components/Heading/index.jsx";
 import { viewHistory } from "../../utils/index";
-import UtilityCard from "../../components/UtilityCard/index.jsx";
-import WritingCard from "../../components/WritingCard/index.jsx";
+import { useState, useEffect } from "react";
+import HeadingComponent from "../../components/Heading/index.jsx";
+import WritingCard from "../../components/WritingCard/index";
+import UtilityCard from "../../components/UtilityCard/index";
+import styled from "styled-components";
+import { Button } from "antd";
+import { Link } from "react-router-dom";
+import { PlusOutlined } from "@ant-design/icons";
 
-const StyledWritingPage = styled.div`
-	display: flex;
-	flex-direction: column;
-	background-color: #f2f2f2;
-	align-items: center;
+const StyledWritingHistoryPage = styled.div`
+  display: flex;
+  flex-direction: column;
+  background-color: #f2f2f2;
+  align-items: flex-start;
 `;
 
 const UtilityCardsWrapper = styled(Link)`
@@ -19,53 +20,87 @@ const UtilityCardsWrapper = styled(Link)`
   display: flex;
   justify-content: flex-start;
   height: 250px;
+  color: ${(props) => props.theme.primaryColor};
 `;
 
-const WritingCardWrapper = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: space-between;
+const StyledCardContainer = styled.div`
+  && {
+    text-align: left;
+    padding-top: 20px;
+    padding-left: 20px;
+    padding-right: 20px;
+  }
 `;
 
-const WritingMain = (props) => {
-	const [writings, setWritings] = useState([]);
+const StyledButton = styled(Button)`
+	height: 25px;
+	width: 100px;
+	margin-top: 5px;
+	margin-bottom: 20px;
+	color: #2f71da;
+	border-color: #2f71da;
+	background: none;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	margin-right: 225px;
+	&:hover {
+		color: white;
+		border-color: none;
+	}
+`;
+
+const WritingHistoryPage = () => {
+	const [data, setData] = useState([]);
+	const [loading, setLoading] = useState(false);
+	const [displayCount, setDisplayCount] = useState(8);
 
 	useEffect(() => {
-		async function fetchWritings () {
-			const requestBody = {
-				type: "writingHistory",
-				uid: "333",
-				from: new Date("1970-01-01"),
-				to: new Date()
-			};
-			const response = await viewHistory(requestBody);
-			setWritings(response);
-		}
-
-		fetchWritings();
+		setLoading(true);
+		const fromDate = "1970-01-01";
+		const toDate = new Date().toISOString().slice(0, 10);
+		viewHistory({ type: "writingHistory", from: fromDate, to: toDate, uid: "333" }).then((response) => {
+			if (response.status === 200) {
+				setData(response.data);
+			} else if (response.status === 500) {
+				alert("Something is wrong with network, please retry.");
+			}
+			setLoading(false);
+		});
 	}, []);
 
+	const handleLoadMore = () => {
+		setDisplayCount(displayCount + 8);
+	};
+
 	return (
-		<StyledWritingPage>
-			<HeadingComponent displayValue={"Writing History"} />
-			<UtilityCardsWrapper to="evaluation">
-				<UtilityCard>
-					<PlusOutlined
-						style={{ fontSize: "60px", color: props.theme.defaultColor }}
-					/>
-				</UtilityCard>
-			</UtilityCardsWrapper>
-			<WritingCardWrapper>
-				{writings.map((writing) => (
+		<StyledWritingHistoryPage>
+			<HeadingComponent displayValue={"Writings Main"} />
+
+			<StyledCardContainer>
+				<UtilityCardsWrapper to="evaluation">
+					<UtilityCard>
+						<PlusOutlined
+							style={{ fontSize: "60px" }}
+						/>
+					</UtilityCard>
+				</UtilityCardsWrapper>
+				{data.slice(0, displayCount).map((item, index) => (
 					<WritingCard
-						key={writing.writing_id}
-						taskTopic={writing.task_topic}
-						writingContent={writing.writing_content}
+						loading={loading}
+						key={index}
+						taskTopic={item.task_topic}
+						writingContent={item.writing_content}
 					/>
 				))}
-			</WritingCardWrapper>
-		</StyledWritingPage>
+			</StyledCardContainer>
+			{data.length > displayCount && (
+				<StyledButton type="primary" onClick={handleLoadMore}>
+					Load More
+				</StyledButton>
+			)}
+		</StyledWritingHistoryPage>
 	);
 };
 
-export default withTheme(WritingMain);
+export default WritingHistoryPage;
