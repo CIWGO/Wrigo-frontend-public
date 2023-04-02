915ee7c0320224551998bc5629d86d082c675f0b
Chang Liu <changstudy19@gmail.com>
2023-04-03 00:01:35 +0930

    minor

--- src/pages/Payment/index.jsx
+++ src/pages/Payment/index.jsx
@@ -7,23 +7,20 @@ const Payment = () => {
 	const username = localStorage.getItem("username");
 	const handleClick = async () => {
 		try {
-			const response = await axios.post(
-				STRIPE_PAYMENT_URL,
-				{
-					uid,
-					username,
-					token,
-					// testing plan id that charges daily
-					planId: "price_1MqA0CJm2vMPXBBfE8jqo1ZX"
-					// actual plan id that charges monthly
-					// planId: "price_1Ml4JRJm2vMPXBBf8YgX4Aqa"
-				},
-				{
-					headers: {
-						"Content-Type": "application/json"
-					}
+			const response = await paymentRedirection({
+				uid,
+				username,
+				token,
+				// testing plan id that charges daily
+				planId: "price_1MqA0CJm2vMPXBBfE8jqo1ZX"
+				// actual plan id that charges monthly
+				// planId: "price_1Ml4JRJm2vMPXBBf8YgX4Aqa"
+			},
+			{
+				headers: {
+					"Content-Type": "application/json"
 				}
-			);
+			});
 			if (response.status === 200) {
 				const { url } = response.data;
 				window.location.href = url;
