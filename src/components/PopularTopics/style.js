4e2a653527718a207e2934ca15a299cc6d749bab
leo <leoyh.97@gmail.com>
2023-04-08 13:55:56 +0800

    connected topics in popular topic dashboard component to individual topic content page; fixed popular topic styles.

--- src/components/PopularTopics/style.js
+++ src/components/PopularTopics/style.js
@@ -8,10 +8,9 @@ export const ContentBox = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  padding: 10px 30px;
-  // position: absolute;
+  padding: 25px 40px;
   background: #ffffff;
-  border-radius: 10px;
+  border-radius: 20px;
 `;
 
 export const Property = styled.div`
@@ -29,36 +28,57 @@ export const IndexAndTopic = styled.div`
   align-items: flex-start;
   justify-content: flex-start;
   gap: 5px;
+  cursor: pointer;
 `;
 
-export const Index = styled.span`
-  color: blue;
+export const Index = styled.div`
+  color: #2F71DA;
   font-weight: bold;
-  padding-right: 10px;
+  padding-right: 5px;
+  padding-top: 1px
 `;
 
-export const TopicContent = styled.span`
-  @import url("https://fonts.googleapis.com/css2?family=Noto+Sans:wght@700&display=swap");
-  font-family: "Noto Sans", sans-serif;
+export const TopicContent = styled.div`
+  @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@200,300;500;700;900&family=Rubik:wght@400;500;600&display=swap');
+
+  font-family: 'Poppins', sans-serif;
+  font-weight: 500;
   display: inline;
+  line-height: 20px;
   margin: 0;
   text-align: left;
 `;
 
 export const IconContainer = styled.div`
+  display: grid;
+  width: 90%;
+  grid-template-columns: 70% auto;
+  gap: 20px;
+  color: #2F71DA;
+  margin-top: 5px;
+  justify-content: flex-start;
+`;
+
+export const IconsWrapper = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  gap: 20px;
+`;
+
+export const Icon = styled.div`
   display: flex;
   align-items: center;
-  gap: 5px;
-  color: blue;
-  margin-top: 5px;
-  justify-content: flex-end;
-  width: 90%;
+  margin-right: 5px;
+
 `;
 
 export const MoreInfo = styled(Link)`
-  font-weight: bold;
-  font-weight:200;
-  font-weight: bold;
+  @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@200,300;500;700;900&family=Rubik:wght@400;500;600&display=swap');
+
+  font-family: 'Poppins', sans-serif;
+  color: #2F71DA;
+  font-weight: 700;
   text-align: left;
   width: 100%;
+  margin-bottom: 5px;
 `;
