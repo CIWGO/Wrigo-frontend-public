4f227dec89c2e7c1104eb108b37014c54ca8e0e1
Chang Liu <changstudy19@gmail.com>
2023-04-10 21:55:31 +0930

    finish terms and conditions

---
+++ src/constants/terms.js
@@ -0,0 +1,47 @@
+export const terms = `Welcome to Wrigo! These Terms and Conditions ("Terms") govern your use of the Wrigo mobile application and website ("App"), operated by The Wrigo Team ("us", "we", or "our"). By accessing or using the App, you agree to be bound by these Terms. If you do not agree with any part of the Terms, you may not use the App.
+
+1. Eligibility
+You must be at least 18 years old to use the App. By using the App, you represent and warrant that you meet this requirement.
+
+2. License to Use the App
+Subject to your compliance with these Terms, we grant you a limited, non-exclusive, non-transferable, and revocable license to use the App for your personal, non-commercial purposes.
+
+3. Intellectual Property
+The App, including its content, features, and functionality, is and will remain the exclusive property of The Wrigo Team and its licensors. The App is protected by copyright, trademark, and other laws, both in Australia and foreign countries. Our trademarks and trade dress may not be used in connection with any product or service without our prior written consent.
+
+4. User-Generated Content
+By submitting any text, images, or other content through the App ("User Content"), you grant us a worldwide, non-exclusive, royalty-free, sublicensable, and transferable license to use, reproduce, modify, distribute, and display your User Content in connection with the App and our business.
+You represent and warrant that you own or have the necessary rights to submit your User Content and that it does not violate any intellectual property rights, privacy rights, or other rights of any third party.
+
+5. Prohibited Uses
+You may not use the App for any illegal or unauthorized purpose, including but not limited to:
+a. Violating any applicable laws or regulations;
+b. Engaging in any activity that infringes upon the rights of others;
+c. Submitting false, misleading, or offensive information;
+d. Attempting to interfere with, compromise the system integrity, or security of the App;
+e. Circumventing or modifying any security measures implemented by the App;
+f. Decompiling, reverse engineering, or otherwise attempting to obtain the source code of the App.
+
+6. Third-Party Services
+The App may contain links to third-party websites or services that are not owned or controlled by us. We have no control over, and assume no responsibility for, the content, privacy policies, or practices of any third-party websites or services. By using the App, you acknowledge and agree that we shall not be responsible or liable, directly or indirectly, for any damage or loss caused or alleged to be caused by or in connection with the use of or reliance on any such content, goods, or services available on or through any such websites or services.
+
+7. Disclaimer of Warranties
+The App is provided on an "AS IS" and "AS AVAILABLE" basis, without any warranties of any kind, either express or implied, including but not limited to warranties of merchantability, fitness for a particular purpose, or non-infringement. We do not warrant that the App will be uninterrupted, secure, or error-free, that defects will be corrected, or that the App or the server that makes it available are free of viruses or other harmful components.
+
+8. Limitation of Liability
+In no event shall The Wrigo Team, its directors, employees, partners, agents, suppliers, or affiliates be liable for any indirect, incidental, special, consequential, or punitive damages, including without limitation, loss of profits, data, use, goodwill, or other intangible losses, resulting from (i) your access to or use of or inability to access or use the App; (ii) any conduct or content of any third party on the App; (iii) any content obtained from the App; and (iv) unauthorized access, use, or alteration of your transmissions or content, whether based on warranty, contract, tort (including negligence), or any other legal theory, whether or not we have been informed of the possibility of such damage, and even if a remedy set forth herein is found to have failed of its essential purpose.
+
+9. Indemnification
+You agree to defend, indemnify, and hold harmless The Wrigo Team and its licensee and licensors, and their employees, contractors, agents, officers, and directors, from and against any and all claims, damages, obligations, losses, liabilities, costs or debt, and expenses (including but not limited to attorney's fees), resulting from or arising out of a) your use and access of the App, by you or any person using your account and password, or b) a breach of these Terms.
+
+10. Changes to the Terms
+We reserve the right, at our sole discretion, to modify or replace these Terms at any time. If a revision is material, we will provide at least 30 days' notice before any new terms take effect. By continuing to access or use the App after any revisions become effective, you agree to be bound by the revised terms. If you do not agree to the new terms, you are no longer authorized to use the App.
+
+11. Termination
+We may terminate or suspend your access to the App immediately, without prior notice or liability, under our sole discretion, for any reason whatsoever and without limitation, including but not limited to a breach of the Terms. Upon termination, your right to use the App will immediately cease.
+
+12. Governing Law
+These Terms shall be governed and construed in accordance with the laws of Australia, without regard to its conflict of law provisions. Our failure to enforce any right or provision of these Terms will not be considered a waiver of those rights. If any provision of these Terms is held to be invalid or unenforceable by a court, the remaining provisions of these Terms will remain in effect.
+
+13. Contact Us
+If you have any questions about these Terms, please contact us at contact@wrigo.com.au.`;
--- src/pages/SignUp/SignUpForm.jsx
+++ src/pages/SignUp/SignUpForm.jsx
@@ -1,12 +1,16 @@
-import { Form, Input, Button, notification } from "antd";
+import { Form, Input, Button, notification, Checkbox, Modal } from "antd";
 import { MyForm } from "./style";
 import { ERROR_MESSAGES } from "../../constants/errorMessages";
 import { signupUser, sendOTPViaEmail } from "../../utils";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { terms } from "../../constants/terms";
 
 const SignUpForm = () => {
 	const navigate = useNavigate();
 	// Think about refactor it by using redux and redux toolkit.
+	const [open, setOpen] = useState(false);
+
 	const onFinish = async (values) => {
 		try {
 			const response = await signupUser(values);
@@ -31,78 +35,122 @@ const SignUpForm = () => {
 	};
 
 	return (
-		<MyForm name="basic"
-			onFinish={onFinish}>
-			<Form.Item
-				name="username"
-				// label="Username"
-				rules={[
-					{
-						required: true,
-						message: ERROR_MESSAGES.username
-					}
-				]}
-			>
-				<Input placeholder="Username" />
-			</Form.Item>
-
-			<Form.Item
-				name="email"
-				// label="Email"
-				rules={[
-					{
-						required: true,
-						message: ERROR_MESSAGES.email
-					},
-					{
-						type: "email",
-						message: ERROR_MESSAGES.emailFormat
-					}
-				]}
-			>
-				<Input placeholder="Email" />
-			</Form.Item>
-
-			<Form.Item
-				name="password"
-				// label="Password"
-				rules={[
-					{
-						required: true,
-						message: ERROR_MESSAGES.password
-					}
-				]}
-			>
-				<Input.Password placeholder="Password" />
-			</Form.Item>
-
-			<Form.Item
-				name="passwordConfirm"
-				// label="Password"
-				rules={[
-					{
-						required: true,
-						message: ERROR_MESSAGES.passwordConfirm
-					},
-					({ getFieldValue }) => ({
-						validator (_, value) {
-							if (!value || getFieldValue("password") === value) {
-								return Promise.resolve();
-							}
-							return Promise.reject(new Error(ERROR_MESSAGES.passwordMatch));
+		<>
+			<MyForm name="basic"
+				onFinish={onFinish}>
+				<Form.Item
+					name="username"
+					// label="Username"
+					rules={[
+						{
+							required: true,
+							message: ERROR_MESSAGES.username
 						}
-					})
-				]}
-			>
-				<Input.Password placeholder="Confirm Password" />
-			</Form.Item>
+					]}
+				>
+					<Input placeholder="Username" />
+				</Form.Item>
 
-			<Form.Item>
-				<Button type="primary" htmlType="submit">
+				<Form.Item
+					name="email"
+					// label="Email"
+					rules={[
+						{
+							required: true,
+							message: ERROR_MESSAGES.email
+						},
+						{
+							type: "email",
+							message: ERROR_MESSAGES.emailFormat
+						}
+					]}
+				>
+					<Input placeholder="Email" />
+				</Form.Item>
+
+				<Form.Item
+					name="password"
+					// label="Password"
+					rules={[
+						{
+							required: true,
+							message: ERROR_MESSAGES.password
+						}
+					]}
+				>
+					<Input.Password placeholder="Password" />
+				</Form.Item>
+
+				<Form.Item
+					name="passwordConfirm"
+					// label="Password"
+					rules={[
+						{
+							required: true,
+							message: ERROR_MESSAGES.passwordConfirm
+						},
+						({ getFieldValue }) => ({
+							validator (_, value) {
+								if (!value || getFieldValue("password") === value) {
+									return Promise.resolve();
+								}
+								return Promise.reject(new Error(ERROR_MESSAGES.passwordMatch));
+							}
+						})
+					]}
+				>
+					<Input.Password placeholder="Confirm Password" />
+				</Form.Item>
+
+				<Form.Item
+					name="terms"
+					valuePropName="checked"
+					rules={[
+						{
+							validator: (_, value) =>
+								value ? Promise.resolve() : Promise.reject(new Error("Please read and accept Terms and Conditions"))
+						}
+					]}
+				>
+					<Checkbox>
+            I have read the{" "}
+						<a href=""
+							onClick={(e) => {
+								e.preventDefault();
+								setOpen(true);
+							}}>
+              Terms and Conditions
+						</a>
+					</Checkbox>
+				</Form.Item>
+
+				<Form.Item>
+					<Button type="primary" htmlType="submit">
 					Sign up
-				</Button>
-			</Form.Item>
-		</MyForm>
+					</Button>
+				</Form.Item>
+			</MyForm>
+
+			<Modal
+				centered
+				open={open}
+				onOk={() => setOpen(false)}
+				onCancel={() => setOpen(false)}
+				width={1000}
+				cancelButtonProps={{ style: { display: "none" } }}
+			>
+				<div style={{
+					padding: "0 30px"
+				}}>
+					<h3>Terms and Conditions</h3>
+					<p style={{
+						whiteSpace: "pre-wrap"
+					}}>
+						{terms}
+					</p>
+				</div>
+			</Modal>
+		</>
 	);
 };
 
