18b7105c46e19d2fbf8d16efebef48a3d1f87cd3
Zizhen Luo s3839954 <zizhenluo2328@gmail.com>
2023-04-13 00:45:05 +1000

    added button direct to payment logic

--- src/pages/WrigoInfinite/index.jsx
+++ src/pages/WrigoInfinite/index.jsx
@@ -1,9 +1,38 @@
 import React from "react";
+import { paymentRedirection } from "../../utils/API";
 import { ButtonContainer, CardBodyHeader, CardBodyPrice, CardDescriptionHeader, CardDescriptionText, CardFree, CardFreeBody, CardPremium, CardsContainer, Container, DefaultButton, Description, Header, Section, TextContainer, CardPreBody, PreCardBodyHeader, PreCardBodyPrice, PreCardDescriptionHeader, PreCardDescriptionText } from "./style";
 // import styles from "./styles";
 import { ReactComponent as Tick } from "./Checkbox.svg";
 
 function Infinite () {
+	const token = localStorage.getItem("token");
+	const uid = localStorage.getItem("uid");
+	const username = localStorage.getItem("username");
+	const handleClick = async () => {
+		try {
+			const response = await paymentRedirection({
+				uid,
+				username,
+				token,
+				// testing plan id that charges daily
+				planId: "price_1MqA0CJm2vMPXBBfE8jqo1ZX"
+				// actual plan id that charges monthly
+				// planId: "price_1Ml4JRJm2vMPXBBf8YgX4Aqa"
+			},
+			{
+				headers: {
+					"Content-Type": "application/json"
+				}
+			});
+			if (response.status === 200) {
+				const { url } = response.data;
+				window.location.href = url;
+			}
+		} catch (error) {
+			console.log("wrong");
+			console.error(error);
+		}
+	};
 	return (
 		<Section>
 			<Container>
@@ -45,7 +74,7 @@ function Infinite () {
 
 						</CardPreBody>
 						<ButtonContainer>
-							<DefaultButton>Subscribe Now</DefaultButton>
+							<DefaultButton onClick={handleClick}>Subscribe Now</DefaultButton>
 						</ButtonContainer>
 					</CardPremium>
 				</CardsContainer>
