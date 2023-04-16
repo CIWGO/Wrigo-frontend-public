4fa2ca957efaad40972e9fadae4640cc8e978741
rongnanx <xurongnan307@gmail.com>
2023-04-16 22:10:59 +1000

    updated

--- src/pages/Landing/style.js
+++ src/pages/Landing/style.js
@@ -149,7 +149,7 @@ export const Section = styled.section`
   position: relative; 
   width: 100%; 
   min-Height: 80vh; 
-  overflow: hidden 
+  overflow: hidden;
 `;
 
 export const H1 = styled.h1`
@@ -214,7 +214,7 @@ export const AboutSection = styled.div`
 display: flex;
 flex-direction: row;
 justify-content: center;
-align-items: center;
+align-items: stretch;
 margin-bottom: 1rem;
 `;
 
@@ -230,7 +230,7 @@ export const TeamSection = styled.div`
   align-items: center;
   margin-bottom: 1rem;
   padding: 5rem;
-  width: 100%;
+  height: 100%;
 `;
 
 export const DevSection = styled.div`
