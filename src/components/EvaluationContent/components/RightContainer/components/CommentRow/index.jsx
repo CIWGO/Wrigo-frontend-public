70dd1f8a2d851f0736773eea2a6ec6c0fdcc0863
Zizhen Luo s3839954 <zizhenluo2328@gmail.com>
2023-04-16 20:54:00 +1000

    deleted overall and added skeleton for right component

--- src/components/EvaluationContent/components/RightContainer/components/CommentRow/index.jsx
+++ src/components/EvaluationContent/components/RightContainer/components/CommentRow/index.jsx
@@ -3,32 +3,35 @@ import Comment from "./components/Comment";
 import { getTopic } from "../../../../../../utils";
 import styled from "styled-components";
 import { useState, useEffect } from "react";
-import { Card } from "antd";
+import { Card, Skeleton } from "antd";
 
 const StyledCard = styled(Card)`
 	background: white;
 	border: none;
 `;
 
-const StyledDiv = styled.div`
-@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap');
-font-family: "Roboto", sans-serif;
-  font-size: 21px;
-  font-weight: 700;
-  line-height: 32px;
-  text-align: left;
-  color: #2f71da;
-  line-height: 54px;
-`;
+// const StyledDiv = styled.div`
+// @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap');
+// font-family: "Roboto", sans-serif;
+//   font-size: 21px;
+//   font-weight: 700;
+//   line-height: 32px;
+//   text-align: left;
+//   color: #2f71da;
+//   line-height: 54px;
+// `;
 
 const CommentRow = (props) => {
 	const [data, setData] = useState(["Not available"]);
+	const [loading, setLoading] = useState(true);
 	useEffect(() => {
+		setLoading(true);
 		const token = localStorage.getItem("token");
 		const uid = localStorage.getItem("uid");
 		getTopic({ token, uid, type: "oneTopic", topic_id: props.topicId }).then((response) => {
 			if (response.status === 200 && response.data.oneSampleWithFeedback) {
 				setData(response.data.oneSampleWithFeedback);
+				setLoading(false);
 			} else if (response.status === 500) {
 				alert("Something is wrong network, please retry.");
 			}
@@ -36,18 +39,22 @@ const CommentRow = (props) => {
 	}, []);
 	return (
 		<StyledCard>
-			<CriteriaMark criteria="Task Response:" scores={data.sampleScore_TR} />
-			<Comment content={data.sampleFeedback_TR} />
-			<CriteriaMark criteria = "Coherence and Cohesion:" scores={data.sampleScore_CC} />
-			<Comment content={data.sampleFeedback_CC} />
-			<CriteriaMark criteria = "Lexical Resource:" scores={data.sampleScore_LR}/>
-			<Comment content={data.sampleFeedback_LR} />
-			<CriteriaMark criteria = "Grammar Rance and Accuracy:" scores={data.sampleScore_GRA} />
-			<Comment content={data.sampleFeedback_GRA} />
-			<StyledDiv>
+			{ loading && <><Skeleton active/> <Skeleton active/> <Skeleton active/></> }
+			{ !loading && <>
+				<CriteriaMark criteria="Task Response:" scores={data.sampleScore_TR} />
+				<Comment content={data.sampleFeedback_TR} />
+				<CriteriaMark criteria = "Coherence and Cohesion:" scores={data.sampleScore_CC} />
+				<Comment content={data.sampleFeedback_CC} />
+				<CriteriaMark criteria = "Lexical Resource:" scores={data.sampleScore_LR}/>
+				<Comment content={data.sampleFeedback_LR} />
+				<CriteriaMark criteria = "Grammar Rance and Accuracy:" scores={data.sampleScore_GRA} />
+				<Comment content={data.sampleFeedback_GRA} />
+				{/* <StyledDiv>
 				Overall:
 			</StyledDiv>
-			<Comment content={data.sampleFeedback_overall} />
+			<Comment content={data.sampleFeedback_overall} /> */}
+			</>}
+
 		</StyledCard>
 	);
 };
