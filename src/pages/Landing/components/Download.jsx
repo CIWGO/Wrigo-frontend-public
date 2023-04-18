ea83d4276bc2504087b2c45860d90d0c9917282c
leo <leoyh.97@gmail.com>
2023-04-18 16:50:05 +0800

    added download file url

--- src/pages/Landing/components/Download.jsx
+++ src/pages/Landing/components/Download.jsx
@@ -2,38 +2,54 @@ import React from "react";
 import styled from "styled-components";
 import { Button } from "antd";
 import { AppleOutlined, WindowsOutlined } from "@ant-design/icons";
-const DownloadButtonsContainer = styled.div`
-  display: flex;
+const DownloadContainer = styled.div`
+ 	display: flex;
 	flex-direction:column;
-  justify-content: center;
-  align-content: center;
-  height: 400px;
+	justify-content: center;
+	align-content: center;
+	height: 400px;
 	background-color:#F7F7F7;
 	padding: 50px;
-	`;
+`;
+
 const DownloadTitle = styled.h1`
 	font-size: 32px;
 	font-weight: 700;
 	color:rgb(47, 115, 218);
-	padding: 10px 0px 30px 0px;
-	`;
+	text-align: center;
+	margin-bottom: 50px;
+`;
+
+const DownloadButtonContainer = styled.div`
+	display: inline-block;
+`;
 
 const DownloadButton = styled(Button)`
 	background-color:#fff;
 	color:#2F71D5;
-  margin: 20px auto;
 	height:50px;
 	width:200px;
+	margin-left: 20px;
+	margin-right: 20px;
 `;
 
 const Download = () => {
 	return (
-		<DownloadButtonsContainer id="download">
-			<DownloadTitle>Download Wrigo App</DownloadTitle>
-			<DownloadButton type="primary">
-				<AppleOutlined />Download for MacOS</DownloadButton>
-			<DownloadButton type="primary"><WindowsOutlined />Download for Windows</DownloadButton>
-		</DownloadButtonsContainer>
+		<DownloadContainer id="download">
+			<DownloadTitle>Download Wrigo Desktop</DownloadTitle>
+			<DownloadButtonContainer>
+				<a href="https://wrigopublicdownload.s3.ap-southeast-2.amazonaws.com/Wrigo.dmg" download="Wrigo for Mac">
+					<DownloadButton type="primary">
+						<AppleOutlined />Download for MacOS
+					</DownloadButton>
+				</a>
+				<a href="https://wrigopublicdownload.s3.ap-southeast-2.amazonaws.com/Wrigo.dmg" download="Wrigo for Windows">
+					<DownloadButton type="primary">
+						<WindowsOutlined />Download for Windows
+					</DownloadButton>
+				</a>
+			</DownloadButtonContainer>
+		</DownloadContainer>
 	);
 };
 
